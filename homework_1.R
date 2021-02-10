fae931fdf831557191fed1c5beaad29fe56a476e
Umberto Collodel <umberto.collodel@gmail.com>
2021-02-10 12:19:15 +0000

    remove cross-validation ofr computational time: first steps parallelization

--- homework_1.R
+++ homework_1.R
@@ -1,3 +1,8 @@
+# 1) Pre-processing of text data 
+# 2) Method
+# 3) Hyperparameter tuning
+# 4) Length of training data
+
 
 library(tidyverse)
 library(stopwords)
@@ -6,6 +11,11 @@ library(reshape2)
 library(SuperLearner)
 library(tidymodels)
 library(corpus)
+library(xgboost)
+library(future)
+
+
+
 
 # Load raw data ----
 
@@ -118,39 +128,95 @@ tidy_tweets_topwords <- top_500 %>%
 
 
     
-  # Save intermediate files ----
+# Save intermediate files ----
     
-    tidy_tweets_topwords %>% 
+tidy_tweets_topwords %>% 
       walk2(name_files, ~ saveRDS(.x, file = paste0("../Machine_learning_for_economics_material/intermediate_data/homework_1/",.y,"_top500_words.rds")))
  
+ 
+# Create a training sample and validation sample of the insample tweets
+
+ set.seed(123)
+
+ 
+train <- tidy_tweets_topwords_train %>% 
+  sample_frac(.7) %>% 
+  as_tibble()
+
+test <- tidy_tweets_topwords_train %>% 
+  filter(!id %in% train$id) %>% 
+  as_tibble()
+
+train_x <- train %>% 
+  select(-id, -Author)
+
+train_y <- train %>% 
+  select(Author) %>% 
+  mutate(Author = case_when(Author == "bernie" ~ 1,
+                            T~ 0))
+
+test_x <- test %>% 
+  select(-id, -Author)
+
+test_y <- test %>%
+  select(Author) %>% 
+  mutate(Author = case_when(Author == "bernie" ~ 1,
+                            T~ 0))
+ 
   
-  
-  # Train the model ----
-  
-  set.seed(243)
-  
-  a <- SuperLearner(Y= ifelse(tidy_tweets_topwords_train$Author == "bernie",1,0), 
-               X= tidy_tweets_topwords_train %>% select(-id,-Author),
+# Train the model ----
+
+length(availableWorkers())
+
+options(mc.cores = 2)
+
+
+set.seed(123, "L'Ecuyer-CMRG")
+
+model <- CV.SuperLearner(Y= train_y$Author, 
+               X= train_x,
                family = binomial(),
                SL.library = c("SL.mean",
-                              "SL.kernelKnn"))
+                              "SL.kernelKnn",
+                              "SL.glmnet",
+                              "SL.randomForest",
+                              "SL.xgboost"),
+               parellel = "multicore",
+               cvControl = list(0))
 
-  predicted <- predict(a, tidy_tweets_topwords_test %>% select(-id)) 
 
-  data.frame(fitted_knn = predicted$library.predict$SL.kernelKnn_All) %>% 
-  ggplot(aes(x=fitted)) +
-    geom_density() +
-    theme_minimal()
-    
+predicted <- predict(model, test_x, onlySL = F)[["library.predict"]] %>% 
+  as_tibble()
+
+names(predicted) %>% 
+  map(~ ROCR::prediction(predicted[,.x], test_y$Author)) %>%
+  map(~ ROCR::performance(.x, measure = "auc", x.measure = "cutoff")@y.values[[1]]) %>% 
+  map(~ data.frame(auc = .x)) %>% 
+  bind_rows() %>% 
+  mutate(model = names(predicted)) %>% 
+  mutate(model = str_extract(model, "(?<=\\.)(.+)(?=_)")) %>%
+  mutate(model = fct_reorder(model, auc, mean)) %>% 
+  ggplot(aes(auc, model)) +
+  geom_col(width = 0.2) +
+  theme_minimal() +
+  xlab("Area under the Curve (AUC)") +
+  ylab("Algorithm")
+
+
+
+
+
+
   
 # Grid search hyperparameters: ---- 
   
-learner = create.Learner("SL.kernelKnn", tune = list(k = c(2:12)))
+learner = create.Learner("SL.kernelKnn", tune = list(k = c(2:8)))
 
 cv_sl = SuperLearner(Y= ifelse(tidy_tweets_topwords_train$Author == "bernie",1,0), 
                      X= tidy_tweets_topwords_train %>% select(-id,-Author),
                      family = binomial(),
-                     SL.library = learner$names)
+                     SL.library = learner$names,
+                     cvControl = list(5))
   
 cv_sl$cvRisk %>% 
   stack() %>% 
