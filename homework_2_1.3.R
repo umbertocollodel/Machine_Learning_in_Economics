e1e990ec47bcc1d61075ca73317a4efe03badc28
Umberto Collodel <umberto.collodel@gmail.com>
2021-02-23 16:46:03 +0000

    finished all estimations and export

--- homework_2_1.3.R
+++ homework_2_1.3.R
@@ -1,12 +1,19 @@
 ###### INSTRUCTIONS TO RUN -----
 
 # Change data_path to path of local raw data files
+# Running time: approx. ...
 
 ######
 
 # Load all required packages -----
 
+packages= c("tidyverse","SuperLearner", "clusterGeneration", "mvtnorm", "xgboost",
+            "stargazer")
 
+lapply(packages, function(x){
+  do.call("require", list(x))
+}
+)
 
 
 # Read the data ----
@@ -19,9 +26,14 @@ df <-  read.csv(data_path) %>%
 
 # Double ML ----
 
+set.seed(245)
 
-k2ml(df$treat_neighbors, df %>% select(-outcome_voted,-treat_neighbors) %>% as.matrix(), df$outcome_voted,
-     family.X = binomial(), family.Y = binomial())
+doubleml <- doubleml(df$treat_neighbors, df %>% select(-outcome_voted,-treat_neighbors) %>% as.matrix(), df$outcome_voted,
+     family.X = binomial(), family.Y = binomial()) %>% 
+  stack() %>% 
+  spread(ind,values) %>% 
+  setNames(c("Estimate","Std. Error")) %>% 
+  mutate(Estimation = "Double ML (XGBoost)")
 
 # OLS without controls ----
 
@@ -29,7 +41,10 @@ lm_no_controls <- lm(outcome_voted ~ treat_neighbors, df) %>%
   summary() %>% 
   coef() %>%
   data.frame() %>% 
-  slice(2)
+  slice(2) %>% 
+  rename(`Std. Error` = `Std..Error`) %>% 
+  select(Estimate, `Std. Error`) %>% 
+  mutate(Estimation = "OLS with no controls")
 
 
 # OLS with controls ----
@@ -48,5 +63,32 @@ lm_controls <- lm(formula, df) %>%
   summary() %>% 
   coef() %>%
   data.frame() %>% 
-  slice(2)
-  
+  slice(2) %>% 
+  rename(`Std. Error` = `Std..Error`) %>% 
+  select(Estimate, `Std. Error`) %>% 
+  mutate(Estimation = "OLS with controls")
+
+
+
+
+# Bind results together and export: -----
+
+export_path_tables="../Machine_learning_for_economics_material/output/homework_2/tables/"
+
+rbind(lm_no_controls, lm_controls, doubleml) %>%
+  select(estimation, Estimate, `Std. Error`) %>%
+  mutate(`95% - Lower` = Estimate - 1.96*`Std. Error`,
+         `95% - Upper `= Estimate + 1.96*`Std. Error`) %>% 
+  stargazer(summary = F,
+            rownames = F,
+            out = paste0(export_path_tables,"comparison_voter_estimation.tex"))
+
+
+
+
+
+
+         
+         
+         
+         
