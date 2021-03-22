48eb2b08640c452840c7614d9eac1174862f27eb
Umberto Collodel <umberto.collodel@gmail.com>
2021-03-22 10:50:53 +0000

    worked on better visualization; re-starting work on honest trees

--- homework_3.R
+++ homework_3.R
@@ -29,12 +29,16 @@ library(causalTree)
 
 # Applying Causal Trees ----
 
-# Read data: we keep only half of the original observations to reduce 
+
+set.seed(345)
+
+# Read data: we keep only two-thirds of the original observations to reduce 
 # computational time
 
 raw_path="../Machine_learning_for_economics_material/raw_data/homework 3/welfare.rds"
+
 df=readRDS(raw_path) %>% 
-  slice(sample(nrow(.),nrow(.)/2))
+  slice(sample(nrow(.),nrow(.)*2/3))
 
 
 # Get formula
@@ -45,7 +49,7 @@ names_independent <- df %>%
 
 tree_fml=as.formula(paste("y", paste(names_independent, collapse = ' + '), sep = " ~ "))
 
-# Run causal tree
+# Run causal tree and plot result:
 
 causal_tree <- causalTree::causalTree(formula = tree_fml,
                           data = df,
@@ -69,54 +73,39 @@ rpart.plot(causal_tree, roundint = F)
 optimal_cp = causal_tree$cptable[which.min(causal_tree$cptable[,4]),1]
 
 pruned_tree <- prune(causal_tree, optimal_cp)
-rpart.plot(pruned_tree,roundint = F)
+rpart.plot(pruned_tree,
+           type = 3,
+           clip.right.labs = F,
+           branch = F,
+           roundint = F)
 
-# Run Honest tree -----
 
-set.seed(12)
+# Honest Tree ----
 
-# Split the data
 
-index=sample(1:nrow(df), nrow(df)/2)
+index=sample(1:nrow(df), nrow(df)*2/3)
 
 df1=df[index,]
 df2=df[-index,]
 
 
-# Run the honest tree
-
-honest_tree <- honest.causalTree(formula = tree_fml,
-                                 data = df1,
-                                 treatment = df1$w,
-                                 est_data = df2,
-                                 est_treatment = df2$w,
-                                 split.alpha = 0.5,
-                                 split.Rule = "CT",
-                                 split.Honest = T,
-                                 cv.alpha = 0.5,
-                                 cv.option = "CT",
-                                 cv.Honest = T,
-                                 split.Bucket = T,
-                                 bucketNum = 5,
-                                 bucketMax = 100, # maximum number of buckets
-                                 minsize = 250) # number of observations in treatment and control on leaf
-
-rpart.plot(honest_tree, roundint = F)
 
 
-# As before, tree too deep and complex: prune with optimal cross-validation cp
+causal_tree <- causalTree::causalTree(formula = tree_fml,
+                                      data = df1,
+                                      treatment = df$w,
+                                      split.Rule = "CT", #causal tree
+                                      split.Honest = F, #will talk about this next
+                                      split.alpha = 1, #will talk about this next
+                                      cv.option = "CT",
+                                      cv.Honest = F,
+                                      split.Bucket = T, #each bucket contains bucketNum treated and bucketNum control units
+                                      bucketNum = 5, 
+                                      bucketMax = 100, 
+                                      minsize = 250) 
 
 
-optimal_cp = honest_tree$cptable[which.min(honest_tree$cptable[,4]),1]
-
-pruned_tree <- prune(honest_tree, optimal_cp)
-rpart.plot(pruned_tree,roundint = F)
-
-
-
-
-
-
+rpart.plot(causal_tree, roundint = F)
 
 
 
