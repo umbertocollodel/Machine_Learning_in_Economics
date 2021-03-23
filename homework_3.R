ebf526b8ce6145b53fcddd3e77c2fcd63c700a9d
Umberto Collodel <umberto.collodel@gmail.com>
2021-03-23 09:28:25 +0000

    completed work on honest tree

--- homework_3.R
+++ homework_3.R
@@ -60,41 +60,43 @@ names_independent <- df %>%
 
 tree_fml=as.formula(paste("y", paste(names_independent, collapse = ' + '), sep = " ~ "))
 
-# Run causal tree and plot result:
+# Run honest tree and plot results:
 
-causal_tree <- causalTree::causalTree(formula = tree_fml,
-                          data = df,
-                          treatment = df$w,
-                          split.Rule = "CT", #causal tree
-                          split.Honest = F, #will talk about this next
-                          split.alpha = 1, #will talk about this next
+honest_tree <- honest.causalTree(formula = tree_fml,
+                          data = df1,
+                          treatment = df1$w,
+                          est_data = df2,
+                          est_treatment = df2$w,
+                          split.Rule = "CT", 
+                          split.Honest = T, 
+                          split.alpha = 0.5,
                           cv.option = "CT",
-                          cv.Honest = F,
-                          split.Bucket = T, #each bucket contains bucketNum treated and bucketNum control units
+                          cv.Honest = T,
+                          split.Bucket = T, 
                           bucketNum = 5, 
                           bucketMax = 100, 
                           minsize = 250) # number of observations in treatment and control on leaf
 
-rpart.plot(causal_tree, roundint = F)
+rpart.plot(honest_tree, roundint = F)
 
 
-# Tree too deep and complex: prune with optimal cross-validation cp
+# Try to prune to enhance readability and avoid overfitting:
 
 
-optimal_cp = causal_tree$cptable[which.min(causal_tree$cptable[,4]),1]
+optimal_cp = honest_tree$cptable[which.min(honest_tree$cptable[,4]),1]
 
-pruned_tree <- prune(causal_tree, optimal_cp)
+pruned_tree <- prune(honest_tree, optimal_cp)
 rpart.plot(pruned_tree,
            type = 3,
            clip.right.labs = F,
            branch = F,
+           cex = 1,
            roundint = F)
 
 # Export:
 
 export_path_figures="../Machine_learning_for_economics_material/output/homework_3/figures/"
-
-ggsave(paste0(export_path,"causal_tree_pruned.pdf"))
+ggsave(paste0(export_path,"honest_tree_pruned.pdf"))
 
 
 
